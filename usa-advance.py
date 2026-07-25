24849b36542d7f1eda9d5ba6335aedf6f4cce556
text2027mail <text2027mail@gmail.com>
2026-07-25 22:56:55 +0530

    Update usa-advance.py

--- usa-advance.py
+++ usa-advance.py
@@ -22,7 +22,7 @@ FETCH_TOMORROW = True
 
 # --- Dates for which we want ALL movies (target languages) ---
 SCRAPE_DATES = [
-    # date(2026, 7, 20),
+     date(2026, 8, 8),
 ]
 
 # --- Custom movies: list of {movie_id, date} ---
